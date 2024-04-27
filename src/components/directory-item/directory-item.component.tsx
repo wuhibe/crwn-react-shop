abdba6aebc818f8baae8afd789c39de42b288da8
wuhibe <wuhibetamire710@gmail.com>
2024-04-27 21:07:17 +0300

    feat: navigate to shop pages from home directory cards

--- src/components/directory-item/directory-item.component.tsx
+++ src/components/directory-item/directory-item.component.tsx
@@ -1,23 +1,31 @@
 import './directory-item.styles.scss'
+import { useNavigate } from 'react-router-dom'
 
 interface DirectoryItemProps {
   title: string
   imageUrl: string
 }
 
-const DirectoryItem = ({ title, imageUrl }: DirectoryItemProps) => (
-  <div className='directory-item-container'>
+const DirectoryItem = ({ title, imageUrl }: DirectoryItemProps) => {
+  const navigate = useNavigate()
+
+  return (
     <div
-      className='background-image'
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
-    <div className='directory-body-container'>
-      <h2>{title}</h2>
-      <p>SHOP NOW</p>
+      className='directory-item-container'
+      onClick={() => navigate(`/shop/${title}`)}
+    >
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className='directory-body-container'>
+        <h2>{title.toUpperCase()}</h2>
+        <p>SHOP NOW</p>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default DirectoryItem
